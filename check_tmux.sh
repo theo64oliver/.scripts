fdad7961bdd1687b2ca8828593df85d11eb68ee8
Theo OLIVER <theo.oliver@epita.fr>
2025-03-19 11:48:48 +0100

    feat: Modify gp to handle specific files, also modif check tmux script

--- check_tmux.sh
+++ check_tmux.sh
@@ -3,8 +3,13 @@
 if [[ "$1" == cd* ]]; then
   dir_name=$(basename "$PWD" | tr . _)
 
-  if tmux has-session -t "$dir_name" 2>/dev/null; then
+  if tmux has-session -t "$dir_name" 2>/dev/null && [ -z "$TMUX" ]; then
     echo Attaching the tmux session "$dir_name"
     tmux attach -t "$dir_name"
+  fi
+
+  if ! tmux has-session -t "$dir_name" 2>/dev/null && [ -z "$TMUX" ] && [ -d "$dir_name/.git" ]; then
+    echo Creating a new tmux session "$dir_name"
+    tmux new-session -s "$dir_name"
   fi
 fi
--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,31 @@
 #! /bin/sh
 
-if [ "$#" -ne 1 ]; then 
-	echo "gp : Use as : gp <message>"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+RED="\e[31m"
+
+msg="$1"
+
+if [ "$msg" = "help" ]; then
+	echo "You need help, let me help you"
+	# Help status, send the help page of gp
+	cat ~/.scripts/help_gp.txt
+	exit 0
+elif [ "$#" -lt 2 ]; then 
+	echo "gp: Use: gp <message> [<files_to_add>]"
 else
-	git add *
-	git commit -m "$1"
+	if [ "$#" -eq 2 ]; then
+		git add *
+	else
+		shift 2
+		for i in "$@"; do 
+			git add "$i"
+		done
+	fi
+
+	git commit -m "$msg"
+	if [ $? -eq 1 ]; then
+		echo -e "${GREEN}Files already committed.${ENDCOLOR}"
+	fi
 	git push
 fi
